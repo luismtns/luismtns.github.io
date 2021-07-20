f453752987df5ebe8bc46b15f0e6b813749dc8ca
Luis  Bovo <luis@fillet.com.br>
2021-07-20 18:36:18 -0300

    try to fix webpack uglify error

--- config/webpack/production.config.js
+++ config/webpack/production.config.js
@@ -1,20 +1,17 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var Dotenv = require('dotenv-webpack');
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var Dotenv = require("dotenv-webpack");
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var MinifyPlugin = require("babel-minify-webpack-plugin");
 
-var appConfig = require('./../App');
-var appEnv = process.env.APP_ENV || 'production';
+var appConfig = require("./../App");
+var appEnv = process.env.APP_ENV || "production";
 
-var sassLoaders = [
-  'css-loader',
-  'sass-loader',
-];
+var sassLoaders = ["css-loader", "sass-loader"];
 
-var orderByList = function(list) {
-  return function(chunk1, chunk2) {
+var orderByList = function (list) {
+  return function (chunk1, chunk2) {
     var index1 = list.indexOf(chunk1.names[0]);
     var index2 = list.indexOf(chunk2.names[0]);
     if (index2 == -1 || index1 < index2) {
@@ -31,42 +28,37 @@ module.exports = {
   devtool: false,
   debug: false,
   sassLoader: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: sassLoaders.join('!')
+    fallback: "style-loader",
+    use: sassLoaders.join("!"),
   }),
   output: {
-    path: path.join(process.cwd(), 'build'),
-    filename: 'assets/bundle-[name].js',
-    chunkFilename: 'assets/bundle-[name]-[chunkhash].js',
-    publicPath: (appConfig.baseUrl + appConfig.path)
+    path: path.join(process.cwd(), "build"),
+    filename: "assets/bundle-[name].js",
+    chunkFilename: "assets/bundle-[name]-[chunkhash].js",
+    publicPath: appConfig.baseUrl + appConfig.path,
   },
   plugins: [
+    new MinifyPlugin(),
     new Dotenv(),
-    new UglifyJSPlugin({
-      parallel: true,
-      uglifyOptions: {
-        ecma: 5,
-      }
-    }),
     new HtmlWebpackPlugin({
-      chunks: ['vendor', 'config', 'app'],
-      chunksSortMode: orderByList(['vendor', 'config', 'app']),
+      chunks: ["vendor", "config", "app"],
+      chunksSortMode: orderByList(["vendor", "config", "app"]),
       inject: true,
       hash: true,
       minify: true,
-      scriptLoading: 'defer',
-      filename: 'index.html',
-      template: 'views/index.html'
+      scriptLoading: "defer",
+      filename: "index.html",
+      template: "views/index.html",
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
-        'APP_ENV': JSON.stringify(appEnv)
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        APP_ENV: JSON.stringify(appEnv),
+      },
     }),
     new ExtractTextPlugin({
-      filename: 'assets/[name]-[contenthash].css',
+      filename: "assets/[name]-[contenthash].css",
       allChunks: true,
-    })
-  ]
+    }),
+  ],
 };
